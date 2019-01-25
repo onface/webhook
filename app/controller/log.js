5385c8224bed7f251ed2b702a5dc9b2a6315587c
Nimo Chu <nimo.jser@gmail.com>
2019-01-25 17:44:21 +0800

    log paging

--- app/controller/log.js
+++ app/controller/log.js
@@ -4,10 +4,24 @@ const uuid = require('uuid')
 class HookController extends Controller {
   async homeRender () {
     const { ctx, app } = this
+    const pageSize = 10
+    let queryPage = ctx.query.page || 1
+    let total = await app.mysql.count('log', {})
+    if (queryPage * pageSize > total) {
+      queryPage = parseInt(total/pageSize) + 1
+    }
+    let resPage = queryPage
     let items = await app.mysql.select('log', {
-      columns: ['id', 'hook_id', 'hook_name', 'request', 'response', 'run_log']
+      columns: ['id', 'hook_id', 'hook_name', 'request', 'response', 'run_log'],
+      limit: pageSize,
+      offset: (queryPage-1) * pageSize
     })
-    await ctx.render('log.html', { items });
+    if (Array.isArray(items)) {
+      if (!items.length) {
+        resPage = 10
+      }
+    }
+    await ctx.render('log.html', { items, total: total, page: resPage });
   }
 }
 
